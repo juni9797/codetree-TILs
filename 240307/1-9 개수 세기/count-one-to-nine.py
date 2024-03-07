de46112665f0e956eb64cf862f2bfc02303cb908
juni9797 <137144440+juni9797@users.noreply.github.com>
2024-03-07 16:06:10 +0900

    Title: 1-9 개수 세기, Time: 126ms, Memory: 24MB, Status: Passed - CodeTree

--- 240307/1-9 개수 세기/count-one-to-nine.py
+++ 240307/1-9 개수 세기/count-one-to-nine.py
@@ -4,9 +4,8 @@ list = list(map(int, input().split()))
 
 cnt_list = [0]*10
 
-for i in range(10):
-    if i in list:
-        cnt_list[i] += 1
+for i in list:
+    cnt_list[i] += 1
 
 for i in range(1, 10):
     print(cnt_list[i])
